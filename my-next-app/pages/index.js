c6365ff2b1299f769cb95cbc6d8bf6dfc7303776
Magicoo51889 <jackot11f@gmail.com>
2022-01-24 19:23:50 +0000

    .gitignore

--- my-next-app/pages/index.js
+++ my-next-app/pages/index.js
@@ -4,7 +4,9 @@ export default class extends Component {
   render () {
     return (
         <div className="homepage-wrapper">
-            <h1>Home page of website</h1>
+            <h1>Jack Foot</h1>
+
+            <h3>This is where I display all of my experience with programming and documents like my CV etc</h3>
         </div>
     )
   }
